f84d45d9eab3874f0754dd653c6718d02b95b9b8
Maximilian <Shawak@users.noreply.github.com>
2020-12-04 12:58:42 +0000

    fix

--- 2020/3/2.rb
+++ 2020/3/2.rb
@@ -3,7 +3,7 @@ h,w=world.size,world[0].size
 tile=->y,x{world[y%h][x%w]}
 p [
     (0...h).zip((0..).step(3)),
-    (0..h).zip(0..),
+    (0...h).zip(0..),
     (0...h).zip((0..).step(5)),
     (0...h).zip((0..).step(7)),
     (0...h).step(2).zip(0..)
