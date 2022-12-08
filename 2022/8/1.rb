913e3bf4ea88f01d28ad745380a3d0d2903d61d1
Maximilian <Shawak@users.noreply.github.com>
2022-12-08 10:44:13 +0100

    Create 1.rb

---
+++ 2022/8/1.rb
@@ -0,0 +1,15 @@
+a=$<.map{|s|s.chomp.chars.map &:to_i}
+r=(1...a.size-1).map{|y|
+    (1...(a[0].size-1)).map{|x|
+        visible=[
+            (x...a.size).drop(1).map{|n|a[y][n]}.all?{|v|v<a[y][x]},
+            x.downto(0).drop(1).map{|n|a[y][n]}.all?{|v|v<a[y][x]},
+            (y...a.size).drop(1).map{|n|a[n][x]}.all?{|v|v<a[y][x]},
+            y.downto(0).drop(1).map{|n|a[n][x]}.all?{|v|v<a[y][x]}
+        ].any? true
+        #p [x, y, visible, a[y][x]]
+        visible ? 1 : 0
+    }
+}
+#p r
+p r.flatten.sum+(a.size*4-4)
