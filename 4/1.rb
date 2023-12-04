283237dc097e79596e338396f0b5ac5053a3d8d8
Maximilian <Shawak@users.noreply.github.com>
2023-12-04 10:26:23 +0100

    Delete 4 directory

--- 4/1.rb
+++
@@ -1,5 +0,0 @@
-r=$<.map{|x|x.split(/:/).map{|s|s.split("|")}.flatten.map(&:strip)}
-p r.map{|e|
-w=e[1].split(" ").map{|s|s.strip.to_i} & e[2].split(" ").map{|s|s.strip.to_i}
-w.size > 0 ? 2**(w.size-1) : 0
-}.sum
